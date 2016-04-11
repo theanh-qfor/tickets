49398c22c02757f5840f91b2b3cad5e50fe502bc
hieu.t.tran <hieu.t.tran@vntrip.vn>
2016-04-12 00:39:09 +0700

    add back include

--- app/Http/Controllers/TicketsController.php
+++ app/Http/Controllers/TicketsController.php
@@ -10,6 +10,7 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Input, Redirect;
 
 
 class TicketsController extends Controller
